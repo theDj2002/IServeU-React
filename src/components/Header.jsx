2ebcbb35bc7b905d2994da2adbe2a2b0158aef43
dibyajyoti.sahoo <dibyajyoti.s@iserveu.co.in>
2026-03-31 11:15:28 +0530

    Initial React project upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <nav className="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
